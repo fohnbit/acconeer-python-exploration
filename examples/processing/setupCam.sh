42c1cb30107f911d311d100ca4975ba3dcd33c9e
fohnbit <unique24@gmx.net>
2019-10-15 14:30:55 +0200

    Update setupCam.sh

--- examples/processing/setupCam.sh
+++ examples/processing/setupCam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gphoto2 --set-config /main/imgsettings/iso=7
+gphoto2 --set-config /main/imgsettings/iso=3
 gphoto2 --set-config aperture=0
 gphoto2 --set-config /main/imgsettings/imageformat=0
