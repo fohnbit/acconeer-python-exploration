5e9a7c56935c1c515067f71d634725be6011a258
fohnbit <unique24@gmx.net>
2019-10-17 08:10:00 +0200

    Update sparse_speed.py

--- examples/processing/sparse_speed.py
+++ examples/processing/sparse_speed.py
@@ -1,29 +1,24 @@
 from enum import Enum
 import numpy as np
 from scipy.signal import welch
-import threading
-import subprocess
-
+import pyqtgraph as pg
+from PyQt5 import QtCore
 
 from acconeer_utils.clients import SocketClient, SPIClient, UARTClient
 from acconeer_utils.clients import configs
 from acconeer_utils import example_utils
+from acconeer_utils.pg_process import PGProcess, PGProccessDiedException
 from acconeer_utils.structs import configbase
 
 
 HALF_WAVELENGTH = 2.445e-3  # m
-NUM_FFT_BINS = 128
+NUM_FFT_BINS = 512
 HISTORY_LENGTH = 2.0  # s
 EST_VEL_HISTORY_LENGTH = HISTORY_LENGTH  # s
 SD_HISTORY_LENGTH = HISTORY_LENGTH  # s
 NUM_SAVED_SEQUENCES = 10
 SEQUENCE_TIMEOUT_COUNT = 10
 
-waitForCompletingSpeedLimitDetection = None
-
-# Speedlimit in km/h
-speedLimit = 4
-speedLimitTemp = speedLimit
 
 def main():
     args = example_utils.ExampleArgumentParser(num_sens=1).parse_args()
@@ -42,7 +37,10 @@ def main():
     sensor_config.sensor = args.sensors
 
     session_info = client.setup_session(sensor_config)
-    print(session_info)
+
+    pg_updater = PGUpdater(sensor_config, processing_config, session_info)
+    pg_process = PGProcess(pg_updater)
+    pg_process.start()
 
     client.start_streaming()
 
@@ -51,39 +49,25 @@ def main():
 
     processor = Processor(sensor_config, processing_config, session_info)
 
-    global speedLimitTemp
-    global waitForCompletingSpeedLimitDetection
-    lastSpeed = 0
-    
     while not interrupt_handler.got_signal:
         info, sweep = client.get_next()
         plot_data = processor.process(sweep)
-        
-        speed = (plot_data["speed"]) * 3.6
-        distance = (plot_data["distance"])
-        
-        if speed > 1 and lastSpeed != speed:
-            print ("Speed: " + str(speed) + " m/s, Distance: " + str(distance))
-            lastSpeed = speed
-        
-        if speed > speedLimitTemp:
-            speedLimitTemp = speed
-            print ("Maximal current Speed: " + str(speedLimitTemp))
-            # if not waitForCompletingSpeedLimitDetection:
-                # waitForCompletingSpeedLimitDetection = True
-                # timer1 = threading.Timer(0.1, captureImageFromCamera) 
-                # timer1.start()
-                # timer2 = threading.Timer(10.0, sendSpeedCatImage)
-                # timer2.start()
+
+        if plot_data is not None:
+            try:
+                pg_process.put_data(plot_data)
+            except PGProccessDiedException:
+                break
 
     print("Disconnecting...")
+    pg_process.close()
     client.disconnect()
 
 
 def get_sensor_config():
     config = configs.SparseServiceConfig()
 
-    config.range_interval = [2.00, 2.20]
+    config.range_interval = [0.30, 0.48]
     config.stepsize = 3
     config.sampling_mode = configs.SparseServiceConfig.SAMPLING_MODE_A
     config.number_of_subsweeps = NUM_FFT_BINS
@@ -183,8 +167,6 @@ class Processor:
         self.sequence_vels = np.zeros(NUM_SAVED_SEQUENCES)
         self.update_idx = 0
 
-        self.depths = get_range_depths(sensor_config, session_info)
-        
         self.update_processing_config(processing_config)
 
     def update_processing_config(self, processing_config):
@@ -233,14 +215,6 @@ class Processor:
         if est_vel < self.min_speed:  # evaluates to false if nan
             est_vel = np.nan
 
-        # print speed and distance
-        fft = np.fft.rfft(zero_mean_sweep.T * np.hanning(sweep.shape[0]), axis=1)
-        abs_fft = np.abs(fft)
-        max_depth_index, max_bin = np.unravel_index(abs_fft.argmax(), abs_fft.shape)
-        depth = self.depths[max_depth_index]
-       
-        # print ("Speed: " + str(est_vel) + " m/s, Distance: " + str(depth))
-        
         # Sequence
 
         self.belongs_to_last_sequence = np.roll(self.belongs_to_last_sequence, -1)
@@ -265,7 +239,7 @@ class Processor:
         self.est_vel_history[-1] = est_vel
 
         if np.all(np.isnan(self.est_vel_history)):
-            output_vel = 0
+            output_vel = None
         else:
             output_vel = np.nanmax(self.est_vel_history)
 
@@ -278,18 +252,183 @@ class Processor:
             self.min_threshold, np.max(nasd_temporal_max) * self.dynamic_threshold)
 
         self.update_idx += 1
-      
+
         return {
             "sweep": sweep,
             "sd": nasd_temporal_max,
             "sd_threshold": temporal_max_threshold,
             "vel_history": self.est_vel_history,
             "vel": output_vel,
-            "speed": est_vel,
-            "distance": depth,
             "sequence_vels": self.sequence_vels,
             "belongs_to_last_sequence": self.belongs_to_last_sequence,
         }
+
+
+class PGUpdater:
+    def __init__(self, sensor_config, processing_config, session_info):
+        self.processing_config = processing_config
+
+        self.num_subsweeps = sensor_config.number_of_subsweeps
+        self.subsweep_rate = session_info["actual_subsweep_rate"]
+        self.depths = get_range_depths(sensor_config, session_info)
+        self.num_depths = self.depths.size
+        self.est_update_rate = self.subsweep_rate / self.num_subsweeps
+
+        self.bin_vs = np.fft.rfftfreq(NUM_FFT_BINS) * self.subsweep_rate * HALF_WAVELENGTH
+        self.dt = 1.0 / self.est_update_rate
+
+        self.setup_is_done = False
+
+    def setup(self, win):
+        # Data plots
+
+        self.data_plots = []
+        self.data_curves = []
+        for i in range(self.num_depths):
+            title = "{:.0f} cm".format(100 * self.depths[i])
+            plot = win.addPlot(row=0, col=i, title=title)
+            plot.showGrid(x=True, y=True)
+            plot.setYRange(-2**15, 2**15)
+            plot.hideAxis("left")
+            plot.hideAxis("bottom")
+            plot.plot(np.arange(self.num_subsweeps), np.zeros(self.num_subsweeps))
+            curve = plot.plot(pen=example_utils.pg_pen_cycler())
+            self.data_plots.append(plot)
+            self.data_curves.append(curve)
+
+        # Spectral density plot
+
+        self.sd_plot = win.addPlot(row=1, col=0, colspan=self.num_depths)
+        self.sd_plot.setLabel("left", "Normalized PSD (dB)")
+        self.sd_plot.showGrid(x=True, y=True)
+        self.sd_curve = self.sd_plot.plot(pen=example_utils.pg_pen_cycler())
+        dashed_pen = pg.mkPen("k", width=2, style=QtCore.Qt.DashLine)
+        self.sd_threshold_line = pg.InfiniteLine(angle=0, pen=dashed_pen)
+        self.sd_plot.addItem(self.sd_threshold_line)
+
+        self.smooth_max = example_utils.SmoothMax(
+                self.est_update_rate,
+                tau_decay=0.5,
+                tau_grow=0,
+                hysteresis=0.2,
+                )
+
+        # Rolling speed plot
+
+        self.vel_plot = pg.PlotItem()
+        self.vel_plot.setLabel("bottom", "Time (s)")
+        self.vel_plot.showGrid(x=True, y=True)
+        self.vel_plot.setXRange(-EST_VEL_HISTORY_LENGTH, 0)
+        self.vel_max_line = pg.InfiniteLine(angle=0, pen=pg.mkPen("k", width=1))
+        self.vel_plot.addItem(self.vel_max_line)
+        self.vel_scatter = pg.ScatterPlotItem(size=8)
+        self.vel_plot.addItem(self.vel_scatter)
+
+        self.vel_html_fmt = '<span style="color:#000;font-size:24pt;">{:.1f} {}</span>'
+        self.vel_text_item = pg.TextItem(anchor=(0.5, 0))
+        self.vel_plot.addItem(self.vel_text_item)
+
+        # Sequence speed plot
+
+        self.sequences_plot = pg.PlotItem()
+        self.sequences_plot.setLabel("bottom", "History")
+        self.sequences_plot.showGrid(y=True)
+        self.sequences_plot.setXRange(-NUM_SAVED_SEQUENCES + 0.5, 0.5)
+        tmp = np.flip(np.arange(NUM_SAVED_SEQUENCES) == 0)
+        brushes = [pg.mkBrush(example_utils.color_cycler(n)) for n in tmp]
+        self.bar_graph = pg.BarGraphItem(
+                x=np.arange(-NUM_SAVED_SEQUENCES, 0) + 1,
+                height=np.zeros(NUM_SAVED_SEQUENCES),
+                width=0.8,
+                brushes=brushes,
+                )
+        self.sequences_plot.addItem(self.bar_graph)
+
+        self.sequences_text_item = pg.TextItem(anchor=(0.5, 0))
+        self.sequences_plot.addItem(self.sequences_text_item)
+
+        sublayout = win.addLayout(row=2, col=0, colspan=self.num_depths)
+        sublayout.addItem(self.vel_plot, col=0)
+        sublayout.addItem(self.sequences_plot, col=1)
+
+        self.setup_is_done = True
+        self.update_processing_config()
+
+    def update_processing_config(self, processing_config=None):
+        if processing_config is None:
+            processing_config = self.processing_config
+        else:
+            self.processing_config = processing_config
+
+        if not self.setup_is_done:
+            return
+
+        for plot in self.data_plots:
+            plot.setVisible(processing_config.show_data_plot)
+
+        self.sd_plot.setVisible(processing_config.show_sd_plot)
+        self.vel_plot.setVisible(processing_config.show_vel_history_plot)
+
+        self.unit = processing_config.shown_speed_unit
+        speed_label = "Speed ({})".format(self.unit.label)
+        self.sd_plot.setLabel("bottom", speed_label)
+        self.vel_plot.setLabel("left", speed_label)
+        self.sequences_plot.setLabel("left", speed_label)
+        max_vel = self.bin_vs[-1] * self.unit.scale
+        self.sd_plot.setXRange(0, max_vel)
+
+        y_max = max_vel * 1.2
+        self.vel_plot.setYRange(0, y_max)
+        self.sequences_plot.setYRange(0, y_max)
+        self.vel_text_item.setPos(-EST_VEL_HISTORY_LENGTH / 2, y_max)
+        self.sequences_text_item.setPos(-NUM_SAVED_SEQUENCES / 2 + 0.5, y_max)
+
+    def update(self, data):
+        # Data plots
+
+        for i, ys in enumerate(data["sweep"].T):
+            self.data_curves[i].setData(ys)
+
+        # Spectral density plot
+
+        psd_db = 20 * np.log10(data["sd"])
+        psd_threshold_db = 20 * np.log10(data["sd_threshold"])
+        m = self.smooth_max.update(max(2 * psd_threshold_db, np.max(psd_db)))
+        self.sd_plot.setYRange(0, m)
+        self.sd_curve.setData(self.bin_vs * self.unit.scale, psd_db)
+        self.sd_threshold_line.setPos(psd_threshold_db)
+
+        # Rolling speed plot
+
+        vs = data["vel_history"] * self.unit.scale
+        mask = ~np.isnan(vs)
+        ts = -np.flip(np.arange(vs.size)) * self.dt
+        bs = data["belongs_to_last_sequence"]
+        brushes = [example_utils.pg_brush_cycler(int(b)) for b in bs[mask]]
+
+        self.vel_scatter.setData(ts[mask], vs[mask], brush=brushes)
+
+        v = data["vel"]
+        if v:
+            html = self.vel_html_fmt.format(v * self.unit.scale, self.unit.label)
+            self.vel_text_item.setHtml(html)
+            self.vel_text_item.show()
+
+            self.vel_max_line.setPos(v * self.unit.scale)
+            self.vel_max_line.show()
+        else:
+            self.vel_text_item.hide()
+            self.vel_max_line.hide()
+
+        # Sequence speed plot
+
+        hs = data["sequence_vels"] * self.unit.scale
+        self.bar_graph.setOpts(height=hs)
+
+        if hs[-1] > 1e-3:
+            html = self.vel_html_fmt.format(hs[-1], self.unit.label)
+            self.sequences_text_item.setHtml(html)
+
 
 def get_range_depths(sensor_config, session_info):
     range_start = session_info["actual_range_start"]
@@ -297,35 +436,6 @@ def get_range_depths(sensor_config, session_info):
     num_depths = session_info["data_length"] // sensor_config.number_of_subsweeps
     return np.linspace(range_start, range_end, num_depths)
 
-def captureImageFromCamera(): 
-    print("Trigger Canon EOS80D\n")
-    myCmd = './captureImage.sh'
-    subprocess.call([myCmd])
 
-def sendSpeedCatImage(): 
-    print ("Lock radar until image is sendet")
-    global waitForCompletingSpeedLimitDetection
-    global speedLimitTemp
-    global speedLimit   
-
-
-    print("Write max Speed to file: " + str(speedLimitTemp))
-    f = open("speed.txt", "w")
-    f.write(str(round(speedLimitTemp, 1)) + " km/h")
-    f.close()
-    
-    print("Start Postprocessing")
-    myCmd = './postProcessing.sh'
-    subprocess.call([myCmd])
-    
-    print("Send Email with Attachment")
-    myCmd = './sendmail.sh'
-    subprocess.call([myCmd])
-
-    speedLimitTemp = speedLimit
-    waitForCompletingSpeedLimitDetection = None
-
-    print ("Release radar lock")
-    
 if __name__ == "__main__":
     main()
